2ec6e54c7a7117fea96917de4dc6fb01efcb8aa2
David Provost <dp@ob7.us>
2015-10-31 22:51:27 -0800

    removed uneeded functions from when I was calling the js from controller - that couldnt work because I needed to add unique IDS and could only do that inline

--- controller.php
+++ controller.php
@@ -25,27 +25,6 @@ class Controller extends BlockController
 		return t("Select a image and add caption or parallax effect. Originally built as a large header image.");
 	}
 
-	public function registerViewAssets()
-	{
-		$blockID=$this->bID;
-		$pkg = 'parallax_image';
-		$al = \Concrete\Core\Asset\AssetList::getInstance();
-		$al->register('javascript','parallax_function','blocks/parallax_image/parallax.js',array('version'=>'1.0.0','minify'=>false,'combine'=>true),$pkg);
-		$al->registerGroup('parallax_function', array(
-			array('javascript', 'parallax_function')
-			));
-	}
-
-	public function on_page_view()
-	{
-		$html = loader::helper('html');
-		// $parallaxstate=$this->parallaxstate;
-		// if ($parallaxstate) {
-		// 	// $this->addFooterItem($html->javascript('parallax.js','parallax_image'));
-		// 	$this->requireAsset('parallax_function');
-		// }
-	}
-
 	public function view()
 	{
 		$file = File::getByID($this->photoID);
